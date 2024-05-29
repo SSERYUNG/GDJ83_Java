a40533710f31d6c56e361e06f20056946642368d
SSERYUNG <ksr0524@naver.com>
2024-05-29 18:11:11 +0900

    class0529

--- src/com/sse/study3/ex1/GameController.java
+++ src/com/sse/study3/ex1/GameController.java
@@ -2,10 +2,14 @@ package com.sse.study3.ex1;
 
 public class GameController {
 	public void start() {
+
+//		1마리 생성
 		MonsterFactory mf = new MonsterFactory();
 		Monster m = mf.creatMonster();
 		System.out.println(m.name);
+		System.out.println(m.lv);
 
+//		랜덤 생성
 		Monster[] monsters = mf.createMonsterGroup();
 
 		for (int i = 0; i < monsters.length; i++) {
--- src/com/sse/study3/ex1/Rpggame2.java
+++ src/com/sse/study3/ex1/Rpggame2.java
@@ -2,6 +2,9 @@ package com.sse.study3.ex1;
 
 public class Rpggame2 {
 	public static void main(String[] args) {
+
+//		몬스터 1마리 생성, 3마리 생성
+
 		GameController control = new GameController();
 		control.start();
 	}
